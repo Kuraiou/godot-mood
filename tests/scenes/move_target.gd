1b4597f65a077918cc78574e5d4f990ffa681716
Zoey Lome <zoey.lome@avant.com>
2025-03-11 11:42:07 -0500

    feat: add GUT4 unit tests.

---
+++ tests/scenes/move_target.gd
@@ -0,0 +1,12 @@
+@icon("res://addons/mood/icons/script.svg")
+
+extends MoodScript
+
+@export var speed_const := "move_speed"
+
+# Fill this out for what code you want to run during physics processing when the
+# mood machine (AKA finite state machine) is in this current mood (AKA state).
+func _physics_process(delta: float) -> void:
+	print("%s: executing" % name)
+	#print("this shouldn't be happening!")
+	target.position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized() * target.get(speed_const) * delta
---
+++ tests/scenes/player.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var current_stamina := 100.0
+@export var max_stamina := 100
+
+@export var move_speed := 20
+@export var sprint_speed := 50
+
+@export var sprint_machine: MoodMachine
