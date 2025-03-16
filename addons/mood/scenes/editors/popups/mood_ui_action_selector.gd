625624e8976aba89946cd52a9bc272061c660cfb
Zoey Lome <zoey.lome@avant.com>
2025-03-16 15:56:41 -0500

    fix: keep selections constant when search changes list.

--- addons/mood/scenes/editors/popups/mood_ui_action_selector.gd
+++ addons/mood/scenes/editors/popups/mood_ui_action_selector.gd
@@ -3,26 +3,22 @@ extends ConfirmationDialog
 
 @export var item_list: ItemList
 @export var action_search: LineEdit
+@export var selected_items: Array[StringName]
 
 signal on_actions_selected(actions: Array[StringName])
 
 func select_items_by_text(texts: Array[StringName]) -> void:
+	self.selected_items = texts
 	_refresh_items()
-
-	var idx := 0
-	for action: StringName in InputMap.get_actions():
-		if String(action) in texts:
-			item_list.select(idx, false)
-		idx += 1
 
 func _on_confirmed() -> void:
 	var indices = item_list.get_selected_items()
 	var actions := [] as Array[StringName]
+
 	for id in indices:
 		var txt = item_list.get_item_text(id)
 		actions.append(StringName(txt))
-	
-	print("emitting ", actions)
+
 	on_actions_selected.emit(actions)
 
 func _ready() -> void:
@@ -39,7 +35,10 @@ func _refresh_items() -> void:
 		if len(filter) > 0 and not action.begins_with(filter):
 			continue
 
-		item_list.add_item(String(action))
+		var idx := item_list.add_item(String(action))
+
+		if String(action) in selected_items:
+			item_list.select(idx, false)
 
 func _on_canceled() -> void:
 	pass # Replace with function body.
@@ -49,5 +48,14 @@ func _on_custom_action(action: StringName) -> void:
 	queue_free()
 
 func _on_action_search_text_changed(_new_text: String) -> void:
-	print("search changed")
 	_refresh_items()
+
+func _on_item_list_multi_selected(index: int, selected: bool) -> void:
+	var action: String = item_list.get_item_text(index)
+
+	var sn_action := StringName(action)
+
+	if sn_action in selected_items and not selected:
+		selected_items.erase(sn_action)
+	elif sn_action not in selected_items and selected:
+		selected_items.append(sn_action)
